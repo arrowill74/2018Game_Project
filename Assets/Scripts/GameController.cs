b7db328c57d80532bea99a3028af179cc7dac6f6
unknown <vicky85412@gmail.com>
2018-10-22 16:33:51 +0800

    add 'play again' & 'quit' on GameoverScene

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	public string returnSceneName = "SampleScene";
+
+	public void playAgain() {
+		SceneManager.LoadScene(this.returnSceneName);
+	}
+
+	public void quitGame() {
+		// Quit is ignored in the editor
+		Application.Quit();
+	}
+}
