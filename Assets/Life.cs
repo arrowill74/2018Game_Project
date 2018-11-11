3937a970befd8366f151e5b35cb161acfd9bd16c
yenchulin <vicky85412@gmail.com>
2018-11-11 23:47:03 +0800

    Revert "Revert 18 1109healthbar"

---
+++ Assets/Life.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Life : MonoBehaviour {
+    Image img;
+    // Use this for initialization
+    void Start () {
+        img = GetComponent<Image>(); //獲取Image元件
+        InvokeRepeating("TimeInjury",0,1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if he is attack by enemy 
+    }
+
+    void TimeInjury(){
+        img.fillAmount -= 0.01f;
+        if (img.fillAmount == 0) //if player life is over, cancel Invoke
+        {
+            CancelInvoke();
+            Debug.Log("沒血了");
+            SceneManager.LoadScene("GameOverScene"); //Turn to GameOverScene
+        
+        } 
+    }
+}
---
+++ Assets/Scripts/ThrowableController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowableController : MonoBehaviour {
+
+	// public GameObject hand;
+	public int throwForceY = 350;
+	public int throwForceZ = 700;
+	private Rigidbody rigid;
+
+	// Use this for initialization
+	void Start () {
+		rigid = this.gameObject.GetComponent<Rigidbody>();
+		rigid.isKinematic = true;
+	}
+
+	public void releaseItem() {
+		rigid.isKinematic = false;
+		this.transform.parent = null;
+		rigid.AddForce(this.transform.up * this.throwForceY);
+		rigid.AddForce(this.transform.forward * this.throwForceZ);
+	}
+}
