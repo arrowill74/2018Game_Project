ab1207cc044a2d842570b93fb627f7c2e0671c9c
Yuzen <arrowill74@gmail.com>
2018-11-28 19:09:51 +0800

    git tru

--- Assets/Scripts/RandomCouple.cs
+++ Assets/Scripts/RandomCouple.cs
@@ -1,22 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class RandomCouple : MonoBehaviour {
-	private GameObject[] men; 
-	private GameObject[] enemies;
-	private List<int> ranNum;
-	private List<bool> allAlive;
-	private int num;
+	GameObject[] men; 
+	GameObject[] enemies;
+	List<int> ranNum;
+	int num;
 
 	// Use this for initialization
 	void Start () {
 		ranNum = new List<int>();
-		allAlive = new List<bool>();
 		men = (GameObject[])GameObject.FindGameObjectsWithTag("man");
 		enemies = (GameObject[])GameObject.FindGameObjectsWithTag("enemy");
-		
 		for(int i = 0; i < enemies.Length; i ++){
 			do{
 				num = Random.Range(0, men.Length);
@@ -28,31 +24,11 @@ public class RandomCouple : MonoBehaviour {
 			ec.businessManScript = men[ranNum[i]].GetComponent<BusinessManController>();
 			Debug.Log(enemies[i].name+" and "+men[ranNum[i]].name+" are couple.");
 		}
-		for(int i = 0; i < men.Length; i++){
-			allAlive.Add(true);
-		}
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		for(int i = 0; i < men.Length; i++){
-			if(men[i].GetComponent<BusinessManController>().alive){
-				break;
-			}else{
-				allAlive[i] = men[i].GetComponent<BusinessManController>().alive;
-			}
-		}
-		if(checkAllDied()){
-			SceneManager.LoadScene("MissionComplete");
-		}
-	}
-
-	private bool checkAllDied(){
-		foreach(bool manAlive in allAlive){
-			if(manAlive){
-				return false;
-			}
-		}
-		return true;
+		
 	}
 }
--- Assets/Scripts/SampleTarget.cs
+++ Assets/Scripts/SampleTarget.cs
@@ -7,23 +7,19 @@ public class SampleTarget : MonoBehaviour {
         public Transform target;
         public Transform boyfriend;
         private NavMeshAgent agent;
-        GameObject varGameObject;
         void Start()
         {
-            varGameObject = GameObject.Find("Enemy");
-            varGameObject.GetComponent<WanderEnemy>().enabled = false;
             agent = GetComponent<NavMeshAgent>();
         }
         void Update()
         {
             if (Vector3.Distance(target.position, boyfriend.position) < 5.0f)
             {
-                //varGameObject.GetComponent<WanderEnemy>().enabled = false;
                 agent.SetDestination(target.position);
             }
             else
             {
-                varGameObject.GetComponent<WanderEnemy>().enabled = true;
+                //agent.SetDestination(target.position);
             }
         }
 }
