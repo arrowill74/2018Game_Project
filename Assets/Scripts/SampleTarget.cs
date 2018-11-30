96536dcbdb6cc1deb4aa6125fb2933c1d709165f
Yuzen <arrowill74@gmail.com>
2018-11-30 13:08:33 +0800

    Revert "reset"
    This reverts commit 604b65220d718ce05a5015d3274551473ab933fe.

--- Assets/Scripts/SampleTarget.cs
+++ Assets/Scripts/SampleTarget.cs
@@ -7,19 +7,23 @@ public class SampleTarget : MonoBehaviour {
         public Transform target;
         public Transform boyfriend;
         private NavMeshAgent agent;
+        GameObject varGameObject;
         void Start()
         {
+            varGameObject = GameObject.Find("Enemy");
+            varGameObject.GetComponent<WanderEnemy>().enabled = false;
             agent = GetComponent<NavMeshAgent>();
         }
         void Update()
         {
             if (Vector3.Distance(target.position, boyfriend.position) < 5.0f)
             {
+                //varGameObject.GetComponent<WanderEnemy>().enabled = false;
                 agent.SetDestination(target.position);
             }
             else
             {
-                //agent.SetDestination(target.position);
+                varGameObject.GetComponent<WanderEnemy>().enabled = true;
             }
         }
 }
