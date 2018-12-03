80c766e8245db76c59f480b6d7fe093893cfce38
Yuzen <arrowill74@gmail.com>
2018-12-03 15:02:44 +0800

    add the number of remaining men and fix camera

---
+++ Assets/Scripts/MenRemain.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenRemain : MonoBehaviour {
+	private GameObject[] men;
+	private Text canvasNum;
+	private int menNum;
+	private string content;
+	// Use this for initialization
+	void Start () {
+		men = (GameObject[])GameObject.FindGameObjectsWithTag("man");
+		canvasNum = gameObject.GetComponent<Text>();
+		content = "/6";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		menNum = 0;
+		foreach(GameObject man in men){
+			if(man.GetComponent<BusinessManController>().alive){
+				menNum++;
+			}
+		}
+		canvasNum.text = menNum + content;
+	}
+}
