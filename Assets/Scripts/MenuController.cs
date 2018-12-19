43a4ce908c5611bff2c2796d9bdc303c9e5ac370
unknown <vicky85412@gmail.com>
2018-12-19 20:02:18 +0800

    add homepage buttons

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+	
+	public void playGame() {
+		SceneManager.LoadScene("SampleScene");
+	}
+
+	public void openHint() {
+		SceneManager.LoadScene("SampleScene");
+	}
+}
